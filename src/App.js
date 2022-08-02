a5b1692e8d1b563d19f451cbb566ab0e9d7f94b2
Dexter Eromosele <dexter25@my.yorku.ca>
2022-08-02 12:58:16 -0400

    Navbar completed with sass styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
-
+import "./App.css";
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Hero,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <header>
+          <Navbar />
+        </header>
+
+        {/* <Hero /> */}
+      </div>
+      {/* <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer /> */}
+    </div>
+  );
 };
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./navbar.scss";
+import { links } from "../../data";
+import logo from "../../assets/logo.svg";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [windowSize, setWindowSize] = useState(getWindowWidth());
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowWidth());
+    }
+    if (windowSize >= 1028) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, [windowSize]);
+
+  function getWindowWidth() {
+    const { innerWidth } = window;
+    return innerWidth;
+  }
+
+  return (
+    <div className="Navbar">
+      <div className="container">
+        <img src={logo} alt="logo" className="logo" />
+
+        {(show || toggleMenu) && (
+          <div className="menu scale-up-center">
+            <RiCloseLine
+              onClick={() => setToggleMenu(false)}
+              className="close"
+            />
+            <nav>
+              <ul>
+                {links.map((link) => {
+                  return (
+                    <li key={link.id}>
+                      <a href={link.url}>{link.text}</a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </nav>
+            <div className="button-container">
+              <button type="button" className="sign-in">
+                Sign in
+              </button>
+              <button type="button" className="sign-up">
+                Sign up
+              </button>
+            </div>
+          </div>
+        )}
+
+        {!toggleMenu && <RiMenu3Line onClick={() => setToggleMenu(true)} />}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
