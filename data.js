e9ac15830ded4902791ed976bc181ceb3e5064f0
Dexter Eromosele <dexter25@my.yorku.ca>
2022-08-02 12:43:08 -0400

    File structure for setup complete

---
+++ data.js
@@ -0,0 +1,84 @@
+import { google, slack, atlassian, dropbox, shopify } from "./assets";
+
+export const heroHeading = "Let's Build something amazing with GPT-3 OpenAI";
+export const heroText =
+  "Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/whatisgpt",
+    text: "what is GPT?",
+  },
+  {
+    id: 3,
+    url: "/openai",
+    text: "open AI",
+  },
+  {
+    id: 4,
+    url: "/casestudies",
+    text: "case studies",
+  },
+  {
+    id: 5,
+    url: "/library",
+    text: "library",
+  },
+];
+
+export const brands = [
+  {
+    id: 1,
+    img: google,
+    text: "google",
+  },
+  {
+    id: 2,
+    img: slack,
+    text: "slack",
+  },
+  {
+    id: 3,
+    img: atlassian,
+    text: "atlassian",
+  },
+  {
+    id: 4,
+    img: dropbox,
+    text: "dropbox",
+  },
+  {
+    id: 5,
+    img: shopify,
+    text: "shopify",
+  },
+];
+
+export const WISmainFeature = {
+  heading: "What is GPT_3",
+  text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.",
+};
+
+export const WISfeatures = [
+  {
+    id: 1,
+    heading: "Chatbots",
+    text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought.",
+  },
+  {
+    id: 2,
+    heading: "Knowledgebase",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.",
+  },
+  {
+    id: 3,
+    heading: "Education",
+    text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.  ",
+  },
+];
